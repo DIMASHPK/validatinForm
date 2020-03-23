ce4cc6a91dc85eeefa2223b69035a09ae5485493
dimashpk <shupimonstr@gmail.com>
2020-03-24 00:13:53 +0200

    Updates

---
+++ precache-manifest.826b8ed720fb773b1d188cb636577868.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773cf5284cf29bbebeae138fafb41964",
+    "url": "/validatinForm/index.html"
+  },
+  {
+    "revision": "ff15d84dbab6e9b3ac8f",
+    "url": "/validatinForm/static/css/main.969ff15d.chunk.css"
+  },
+  {
+    "revision": "e07fb35dafbc2d4a141f",
+    "url": "/validatinForm/static/js/2.62547687.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/validatinForm/static/js/2.62547687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff15d84dbab6e9b3ac8f",
+    "url": "/validatinForm/static/js/main.ab026f13.chunk.js"
+  },
+  {
+    "revision": "902fdaf5bbedf5131ca7",
+    "url": "/validatinForm/static/js/runtime-main.e8269a25.js"
+  },
+  {
+    "revision": "a95cb88be1095a0cf15b8bd8b8566149",
+    "url": "/validatinForm/static/media/Baloo.a95cb88b.eot"
+  },
+  {
+    "revision": "a95cb88be1095a0cf15b8bd8b8566149",
+    "url": "/validatinForm/static/media/Baloo.a95cb88b.ttf"
+  },
+  {
+    "revision": "a95cb88be1095a0cf15b8bd8b8566149",
+    "url": "/validatinForm/static/media/Baloo.a95cb88b.woff"
+  },
+  {
+    "revision": "5e2ffbd67d8da772f55b87a5cee82d1e",
+    "url": "/validatinForm/static/media/check.5e2ffbd6.svg"
+  }
+]);
